5dd6b5f54eb4eff4f534dfd8c6dfec97f430f4df
daniil <onlybmw17@gmail.com>
2025-07-07 19:45:40 +0300

    babah

--- src/components/Metrika.tsx
+++ src/components/Metrika.tsx
@@ -30,7 +30,6 @@ export function Metrika() {
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
- 
         ym(${counter}, "init", {
           defer: true,
           clickmap:true,
